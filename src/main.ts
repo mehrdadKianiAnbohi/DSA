72a05d6451ace70b8f83dd15eb6641f219855cf9
mehrdadKianiAnbohi <mehrdad.kian71@gmail.com>
2025-03-26 00:24:19 +0330

    implement algoalgorithm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-function greet(name: string): void {
-    console.log(`Hello, ${name}!`);
+function isPalindrome(s: string): boolean {
+    const normalizeString = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    if (normalizeString.length === 0) return false;
+
+    let left = 0;
+    let right = normalizeString.length-1;
+    console.log(Math.ceil(5/2) )
+
+    for (let i = 0; i < Math.ceil(s.length-1); i++) {
+        if (normalizeString[left] !== normalizeString[right]) {
+            return false
+        }
+        left++;
+        right--;
+    }
+
+
+    return true
 }
 
-greet("TypeScript");
+
+isPalindrome("A man, a plan, a canal: Panama")
